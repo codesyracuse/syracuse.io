ac51d87da601b8e921ca27f4ab765fa5d6e83263
Mike Vormwald <mvormwald@gmail.com>
2024-12-13 23:06:22 -0500

    astro add cloudflare

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,8 +3,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwind from '@astrojs/tailwind';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  experimental: { svg: true }
+  experimental: { svg: true },
+  adapter: cloudflare()
 })
